c400cb7e88381ac5adeb2c29526a1676c143d71b
Todd Hogarth <begin-again@users.noreply.github.com>
2019-08-05 09:44:03 -0400

    small refactor

--- tests/user.test.js
+++ tests/user.test.js
@@ -5,14 +5,13 @@ const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const User = require('../src/models/user');
 
-const fakeUser = {
-  name: 'fake user',
-  email: 'fake@example.com'
-};
-
 const fakeID = new mongoose.Types.ObjectId();
 
 describe('New Users', () => {
+  const fakeUser = {
+    name: 'fake user',
+    email: 'fake@example.com'
+  };
   beforeAll(async () => {
     await User.deleteMany();
   });
@@ -40,12 +39,16 @@ describe('New Users', () => {
 });
 
 describe('Login', () => {
+  const fakeUser = {
+    name: 'fake user',
+    email: 'fake@example.com'
+  };
   beforeAll(async () => {
     await User.deleteMany();
-    await new User(fakeUser).save();
   });
   it('should login an existing user', async () => {
     fakeUser.password = '123secret!!';
+    await new User(fakeUser).save();
     await request(app)
       .post('/users/login')
       .send({
